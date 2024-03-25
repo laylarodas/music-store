33de334cd74e51c0c780df7e97da0fc616cbc4f6
Layla Rodas <rodas.layla@gmail.com>
2024-03-25 21:14:37 +0100

    creating custome hook -- useCart

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,10 @@
 import { Header } from "./components/Header"
 import { Product } from "./components/Product"
-import { useState, useEffect } from "react"
-import { db } from "./data/db"
+import { useCart } from './hooks/useCart'
 
 function App() {
 
-  const initialCart = () => {
-    const localData = localStorage.getItem('cart');
-    return localData ? JSON.parse(localData) : [];
-  }
-
-  const [data] = useState(db);
-  const [cart, setCart] = useState(initialCart);
-
-  const MAX_QUANTITY = 5;
-  const MIN_QUANTITY = 1;
-
-  useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }, [cart]);
-
-  function addToCart(item) {
-
-    const itemExists= cart.findIndex(product => product.id === item.id);
-
-    if (itemExists >= 0) {//item exist in cart
-
-      if (cart[itemExists].quantity >= MAX_QUANTITY) {
-        return;
-      }
-
-
-      const updatedCart = [...cart];//copy cart
-
-      updatedCart[itemExists].quantity += 1;//position of item in cart increase quantity
-
-      setCart(updatedCart);//set new cart
-      
-
-    }else{
-      item.quantity = 1;
-      setCart([...cart, item]);
-    }
-
-  }
-
-  function removeFromCart(id) {
-   
-    setCart(prevCart => prevCart.filter(product => product.id !== id));
-  }
-
-  function increaseQuantity(id) {
-    const updatedCart = cart.map(item => {
-      if (item.id === id && item.quantity < MAX_QUANTITY) {
-        return {
-          ...item,
-          quantity: item.quantity + 1
-        }
-      }
-      return item;
-    })
-
-    setCart(updatedCart);
-  }
-
-  function decreaseQuantity(id) {
-    //console.log('decreaseQuantity', id);
-
-    const updatedCart = cart.map(item => {
-      if (item.id === id && item.quantity > MIN_QUANTITY) {
-        return {
-          ...item,
-          quantity: item.quantity - 1
-        }
-      }
-      return item;
-    })
-
-    setCart(updatedCart);
-  }
-
-
-  function clearCart() {
-    setCart([]);
-  }
-
-
-  function saveLocalStorage() {
-    
-  }
+  const { data, cart, addToCart, increaseQuantity, decreaseQuantity, removeFromCart, clearCart } = useCart();
 
   return (
     <>
@@ -106,7 +22,11 @@ function App() {
 
         <div className="row mt-5">
           {data.map((product) => (
-            <Product key={product.id} product={product} setCart={setCart} addToCart={addToCart}/>
+            <Product 
+                  key={product.id} 
+                  product={product}
+                  addToCart={addToCart}
+            />
           ))}
         </div>
       </main>
